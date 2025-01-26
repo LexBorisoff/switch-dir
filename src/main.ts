99528480ccd459e558be84f245bfd96524233bc3
Lex Borisoff <lex@borisoff.dev>
2025-01-26 17:41:44 -0500

    feat: functionality to construct a directory path

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+
+import { selectDir } from './directory/select-dir.js';
+import { updateTmp } from './update-tmp.js';
+import { logger } from './utils/logger.js';
+
+(async function main() {
+  try {
+    const dir = await selectDir();
+
+    if (dir != null) {
+      updateTmp(dir);
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(error.message);
+    }
+  }
+})();
