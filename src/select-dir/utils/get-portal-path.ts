70d9d85ba70c466a2cc61b9b75dcd1dcd6f02943
Lex Borisoff <lex@borisoff.dev>
2025-01-27 21:49:08 -0500

    refactor(select directory): improved getting portal path

--- src/select-dir/utils/get-portal-path.ts
+++ src/select-dir/utils/get-portal-path.ts
@@ -3,42 +3,66 @@ import $_ from '@lexjs/prompts';
 import { getConfigData } from '../../config/get-config-data.js';
 import { colors } from '../../utils/logger.js';
 
+import { suggestMatch } from './suggest-match.js';
+
+function filter(portalArg: string) {
+  return function ([key]: [key: string, value: string]) {
+    const a = portalArg.toLowerCase();
+    const k = key.toLowerCase();
+    return a !== '' && (k === a || k.includes(a));
+  };
+}
+
 export async function getPortalPath(
   portalArg: string,
 ): Promise<string | undefined> {
   const { portals } = getConfigData();
-  let portalPath = portals[portalArg];
 
-  if (portalPath == null) {
-    const matchedPortals = Object.entries(portals).filter(([key]) =>
-      key.includes(portalArg),
-    );
-
-    if (matchedPortals.length === 1) {
-      portalPath = matchedPortals[0][1];
-    } else {
-      const hasMatches = matchedPortals.length > 1;
-      const message = hasMatches
-        ? `Portal argument ${colors.yellow(portalArg)} matches multiple names`
-        : `Portal argument ${colors.red(portalArg)} does not match any name`;
-
-      const { selected } = await $_.autocomplete({
-        name: 'selected',
-        message,
-        choices: Object.entries(portals).map(([key, value]) => ({
-          title: key,
-          value: key,
-          description: value,
-        })),
-      });
-
-      if (selected == null) {
-        return undefined;
-      }
-
-      portalPath = portals[selected];
-    }
+  // exact match
+  if (portalArg !== '' && portals[portalArg] != null) {
+    return portals[portalArg];
   }
 
-  return portalPath;
+  const matchedPortals = Object.entries(portals).filter(filter(portalArg));
+
+  // single match
+  if (matchedPortals.length === 1) {
+    return matchedPortals[0][1];
+  }
+
+  // multiple or no matches
+  const hasMatches = matchedPortals.length > 1;
+  const matchesMessage = hasMatches
+    ? `Portal argument ${colors.yellow(portalArg)} matches multiple names`
+    : `Portal argument ${colors.red(portalArg)} does not match any name`;
+
+  const choices = Object.entries(portals).map(([key, value]) => ({
+    title: key,
+    value: key,
+    description: value,
+  }));
+
+  const { selected } = await $_.autocomplete({
+    name: 'selected',
+    message: portalArg === '' ? `Select portal` : matchesMessage,
+    choices,
+    suggest(input: string | number, list) {
+      const inputStr = `${input}`;
+      let inputArr: string[] = [];
+
+      if (hasMatches || inputStr !== '') {
+        inputArr = inputStr === '' ? [portalArg] : inputStr.split(/\s+/);
+      }
+
+      return Promise.resolve(
+        list.filter(({ value }) => inputArr.every(suggestMatch(value))),
+      );
+    },
+  });
+
+  if (selected == null) {
+    return undefined;
+  }
+
+  return portals[selected];
 }
