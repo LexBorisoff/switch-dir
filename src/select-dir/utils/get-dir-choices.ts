9b14ecd9b6a0a8e525a88ae5061aac0bb2002349
Lex Borisoff <lex@borisoff.dev>
2025-01-28 10:53:41 -0500

    feat(types): created Choice interface with generic value type

--- src/select-dir/utils/get-dir-choices.ts
+++ src/select-dir/utils/get-dir-choices.ts
@@ -2,14 +2,17 @@ import path from 'node:path';
 
 import { getDirNames } from './get-dir-names.js';
 
-import type { Choice } from '@lexjs/prompts/lib';
+import type { Choice } from '../../types/choice.types.js';
 
-export function getDirChoices(dirPath: string, withNav = true): Choice[] {
-  const toChoice = (value: string): Choice => {
+export function getDirChoices(
+  dirPath: string,
+  withNav = true,
+): Choice<string>[] {
+  const toChoice = (value: string): Choice<string> => {
     const description = path.resolve(dirPath, value);
     return { value, title: value, description };
   };
 
-  const choices: Choice[] = getDirNames(dirPath).map(toChoice);
+  const choices: Choice<string>[] = getDirNames(dirPath).map(toChoice);
   return withNav ? ['.', '..'].map(toChoice).concat(choices) : choices;
 }
